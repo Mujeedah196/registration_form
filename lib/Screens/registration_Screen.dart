bf777c4c8078ead34e8dd014a265c7b7f25fa3f6
Mujeedah <mujeedaholaoluwa@gmail.com>
2025-01-23 03:39:37 -0800

    registration form

---
+++ lib/Screens/registration_Screen.dart
@@ -0,0 +1,26 @@
+
+
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  _RegistrationScreenState createState()=> _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen>{
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  bool isRegistered = false;
+}
+
+
+@override
+Widget build (BuildContext context) {
+  return Scaffold(
+    appBar:AppBar(
+      title:  const Text("Registration form"),
+    ) ,
+  );
+}
