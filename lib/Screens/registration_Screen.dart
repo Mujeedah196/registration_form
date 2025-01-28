7b805905e02d29a703b035890230f6d79208b4fe
Mujeedah <mujeedaholaoluwa@gmail.com>
2025-01-28 04:25:23 -0800

    registration form

--- lib/Screens/registration_Screen.dart
+++ lib/Screens/registration_Screen.dart
@@ -18,7 +18,68 @@ Widget build (BuildContext context) {
     appBar:AppBar(
       title:  const Text("Registration form"),
     ) ,
-  );
+  body: Padding(
+    padding: 
+    const EdgeInsets.all(20),
+    child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller:  firstNameController,
+            decoration: InputDecoration(
+              labelText: "First Name"
+            ),
+            ),
+            TextField(
+              controller: lastNameController,
+              decoration: InputDecoration(
+                labelText: "Last Name"
+              ),
+            ),
+          TextField(
+            controller: emailController,
+            decoration: InputDecoration(
+              labelText: "Email Address"
+            ),
+            ),
+           const SizedBox(height: 30,),
+           GestureDetector(
+            onTap: () {
+              setState(() {
+                isRegistered = !isRegistered;
+              });
+            },
+            child: Container(
+             padding:const EdgeInsets.all(20), 
+             decoration: BoxDecoration(
+              color: isRegistered? Colors.green:Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+             ),
+             child: Text(
+              isRegistered?
+              "successful":
+              "Register Me",
+              style:const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+             ),
+            ),
+           )
+
+          
+        ],
+        
+          
+  
+
+            )
+          )
+        
+    ) ;
+
+  
+  
 }
 }
 
